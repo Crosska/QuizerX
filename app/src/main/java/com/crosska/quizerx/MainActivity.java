dcc2e9f1d062de189292559baccde081f0261bb4
Vladislav Muzichin <musical.vg@gmail.com>
2019-03-10 07:41:01 +0300

    1. First Android app.

--- app/src/main/java/com/crosska/quizerx/MainActivity.java
+++ app/src/main/java/com/crosska/quizerx/MainActivity.java
@@ -2,6 +2,8 @@ package com.crosska.quizerx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void false_button_pressed(View view) {
+        Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+    }
+
+    public void true_button_pressed(View view) {
+        Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
     }
 }
