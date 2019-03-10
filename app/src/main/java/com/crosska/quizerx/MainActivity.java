ef08bcdf73d0dbf5c9f380318a2cd166cf97f736
Vladislav Muzichin <musical.vg@gmail.com>
2019-03-10 07:58:54 +0300

    1. First Android app. Additional task: Toast gravity TOP.

--- app/src/main/java/com/crosska/quizerx/MainActivity.java
+++ app/src/main/java/com/crosska/quizerx/MainActivity.java
@@ -1,7 +1,9 @@
 package com.crosska.quizerx;
 
+import android.icu.text.UnicodeSetSpanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,11 +15,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void false_button_pressed(View view) {
-        Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+    public void true_button_pressed(View view) {
+        showToast(1);
     }
 
-    public void true_button_pressed(View view) {
-        Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+    public void false_button_pressed(View view) {
+        showToast(2);
     }
+
+    private void showToast(int ToastType) {
+        switch (ToastType)
+        {
+            case 1:
+                Toast toast_tr = Toast.makeText(getApplicationContext(), R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast_tr.setGravity(Gravity.TOP, 0, 0);
+                toast_tr.show();
+                break;
+            case 2:
+                Toast toast_fl = Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast_fl.setGravity(Gravity.TOP, 0, 0);
+                toast_fl.show();
+                break;
+            default:
+                Toast toast_er = Toast.makeText(getApplicationContext(), R.string.error_toast, Toast.LENGTH_LONG);
+                toast_er.setGravity(Gravity.TOP, 0, 0);
+                toast_er.show();
+                break;
+        }
+    }
+
 }
